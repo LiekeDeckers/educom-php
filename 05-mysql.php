318e1a49774146dbb28fde67dd43b9b7f9b1f016
LiekeDeckers <liekedeckers15@gmail.com>
2023-03-15 12:45:07 +0100

    05 PHP MySQL #5

--- 05-mysql.php
+++ 05-mysql.php
@@ -71,8 +71,99 @@ if ($conn->multi_query($sql) === TRUE) {
 }
 
 // prepared statements and bound parameters
+  // prepare and bind
+$stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $firstname, $lastname, $email);
+
+  // set parameters and execute
+$firstname = "John";
+$lastname = "Doe";
+$email = "john@example.com";
+$stmt->execute();
+
+$firstname = "Mary";
+$lastname = "Moe";
+$email = "mary@example.com";
+$stmt->execute();
+
+$firstname = "Julie";
+$lastname = "Dooley";
+$email = "julie@example.com";
+$stmt->execute();
+
+echo "New records created successfully";
+
+  //close
+$stmt->close();
 
 
+// Select data from table
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+
+// WHERE --> to extract only those records that fulfill a specified condition
+$sql = "SELECT id, firstname, lastname FROM MyGuests WHERE lastname='Doe'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+
+// ORDER BY --> to sort the results
+$sql = "SELECT id, firstname, lastname FROM MyGuests ORDER BY lastname";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+
+// Delete records
+$sql = "DELETE FROM MyGuests WHERE id=3";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record deleted successfully";
+} else {
+  echo "Error deleting record: " . $conn->error;
+}
+
+
+// Update records
+$sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . $conn->error;
+}
+
+// Limit data selections
+$sql = "SELECT * FROM Orders LIMIT 30";
+//or
+$sql = "SELECT * FROM Orders LIMIT 10 OFFSET 15";
+// is the same as
+$sql = "SELECT * FROM Orders LIMIT 15, 10";
 
 $conn->close();
 ?>
